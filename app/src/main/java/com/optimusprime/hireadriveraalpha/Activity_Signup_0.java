3c1d6ac1e31523c84de6c35d2ebf0dc1217690c1
trooper1428 <ruhulremon1428>
2018-10-03 22:30:40 +0600

    signup and login Activities

---
+++ app/src/main/java/com/optimusprime/hireadriveraalpha/Activity_Signup_0.java
@@ -0,0 +1,13 @@
+package com.optimusprime.hireadriveraalpha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Signup_0 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__signup_0);
+    }
+}
